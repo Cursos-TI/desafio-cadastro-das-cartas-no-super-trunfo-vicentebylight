4f18eeb5268f0b5de0d182e851f1e8f594b26e23
vicentebylight <vicentebylight@gmail.com>
2025-03-13 21:53:49 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,33 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+    
+    printf ("Desafio Super Trunfo - Países\n");
+
+    printf ("Carta_01\n");
+
+    char estado [10] = "A";
+    char codigo [10] = "A01\n";
+    char cidade [30] = "Fortaleza\n";
+    int populacao = 200.000;
+    float area = 100.10;
+    int pontos = 25;
+
+    
+printf ("Estado: %s\n Codigo: %s\n Cidade: %s\n população: %d\n Area: %f\n Pontos turisticos: %d\n", estado, codigo, cidade, populacao, area, pontos);
+
+printf ("Carta_02\n");
+
+    char estado [10] = "B";
+    char codigo [20] = "B01\n";
+    char cidade [30] = "Recife\n";
+    int populacao = 300.000;
+    float area = 200.20;
+    int pontos = 50;
+
+    printf ("Estado: %s\n Codigo: %s\n Cidade: %s\n população: %d\n Area: %f\n Pontos turisticos: %d\n", estado, codigo, cidade, populacao, area, pontos);
+
+
+return 0;
+
 }
